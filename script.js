fa388ae2b8ddcb7defc04eeeaa7fa4d824a16ca7
Nora Westphal <tupfen@hotmail.de>
2023-10-12 17:26:33 +0200

    items can be edited

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container"); 
 function addTask(){
         let li = document.createElement("li"); 
-        li.innerHTML= inputBox.value; 
         listContainer.appendChild(li);
+        let p = document.createElement("p")
+        p.innerHTML= inputBox.value; 
+        li.appendChild(p)
+        let span1 = document.createElement("span");
+        span1.classList.add("editSpan") 
+        span1.innerHTML = "&#128393"; 
+        li.appendChild(span1); 
         let span = document.createElement("span"); 
         span.innerHTML = "\u00d7"; 
         li.appendChild(span); 
@@ -17,8 +23,23 @@ listContainer.addEventListener("click", function(e){
         e.target.classList.toggle("checked"); 
         saveData();
         
-    }
-    else if(e.target.tagName === "SPAN"){
+    } else if (e.target.classList[0] === "editSpan") {
+        if (!e.target.classList.value.includes("saveSpan")) {
+          let currentTask = e.target.previousSibling.innerText;
+          e.target.previousSibling.remove();
+          e.target.parentElement.insertAdjacentHTML("afterbegin", `<input type="text" id="input-box" placeholder="${currentTask}">`)
+          e.target.innerHTML = "&#128427"
+          e.target.classList.add("saveSpan")
+      } else {
+        let editedTask = e.target.previousSibling.value;
+        e.target.previousSibling.remove();
+        e.target.parentElement.insertAdjacentHTML("afterbegin", `<p>${editedTask}</p>`)
+        e.target.innerHTML = "&#128393";
+        e.target.classList.remove("saveSpan")
+        saveData();
+
+      }
+    } else if(e.target.tagName === "SPAN"){
             e.target.parentElement.remove(); 
             saveData();
     }
